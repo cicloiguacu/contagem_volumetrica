4099764e7a07f91aca3894265764f9d4f45cb0e4
Joaobazzo <joao.bazzo@gmail.com>
2020-01-27 21:53:29 -0300

    code gender

--- R/2019/total_gender.R
+++ R/2019/total_gender.R
@@ -9,21 +9,26 @@ sname <- openxlsx::getSheetNames("data/2019/contagens2019.xlsx")
 
 
 # centro
-dtcen <- openxlsx::read.xlsx(xlsxFile = "data/2019/contagens2019.xlsx",sheet = sname[9],startRow=8)
-dtcen1 <- openxlsx::read.xlsx(xlsxFile = "data/2019/contagens2019.xlsx",sheet = sname[9+1],startRow=8)
+i = 3
+dtcen <- openxlsx::read.xlsx(xlsxFile = "data/2019/contagens2019.xlsx",sheet = sname[i],startRow=8)
+dtcen1 <- openxlsx::read.xlsx(xlsxFile = "data/2019/contagens2019.xlsx",sheet = sname[i+1],startRow=8)
 dtcen2 <- rbind(dtcen,dtcen1)
 dtcen2[is.na(dtcen2)] <- 0
 
 t_masc <- dtcen2[,which(names(dtcen2) %like% "masc")] %>% sum()
-t_masc
 t_fem <-  dtcen2[,which(names(dtcen2) %like% "fem")] %>% sum()
-t_fem
+t_nid <-  dtcen2[,which(names(dtcen2) %like% "nid")] %>% sum()
 t_bici <- dtcen2[,which(names(dtcen2) %like% "BICI")] %>% sum()
-t_bici
 
-DT <- data.table:as.data.table("name" = sname[9],
-                            "masc" = t_masc,
-                            "fem" = t_fem,
-                            "total" = t_bici)
+DT <- data.table::data.table(name = sname[9],
+                            masc = t_masc,
+                            fem = t_fem,
+                            nid = t_nid,
+                            total = t_bici)
+
+DT
+
+break()
+
 sapply(1:length(names(dtcen)),function(i){which(names(dtcen)[i] %like% c("masc","fem","nid","BICI"))})
        
